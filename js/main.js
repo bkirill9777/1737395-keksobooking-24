f825a5a405a55c3469acd34e7e7304c644677a4e
bkirill9777 <kirillbaranov087@gmail.com>
2021-10-03 20:13:27 +0300

    Исправлена функция getRandomIntInclusive

--- js/main.js
+++ js/main.js
@@ -1,17 +1,14 @@
 function randomNumber(min, max) {
   if (min >= max) {
     console.error('Значение ' + max + ' должно быть больше')
-    return;
+   return;
   }
   return Math.random() * (max - min) + min;
 }
 
 
 function getRandomIntInclusive(min, max) {
-  min = min;
-  max = max;
-  return ((Math.random() * (max - min)) + min).toFixed(5); //Максимум и минимум включаются, Округление до 5 знаков
-
+  return ((Math.random() * (max - min)) + min).toFixed(5); //Максимум и минимум включаются, округление до 5 знаков
+}
 // Источник https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Number/toString
-
