dc61c27d0b8a8acdc6bd7f060c671717cc3bdf90
bkirill9777 <kirillbaranov087@gmail.com>
2021-10-03 21:35:09 +0300

    Исправление ошибок ESlint

--- js/main.js
+++ js/main.js
@@ -1,14 +1,19 @@
+// eslint-disable-next-line no-unused-vars
 function randomNumber(min, max) {
   if (min >= max) {
-    console.error('Значение ' + max + ' должно быть больше')
-   return;
+    /* eslint no-console: "error" */
+    // eslint-disable-next-line no-console
+    console.error(`Значение ${  max  } должно быть больше`);
+    return;
   }
   return Math.random() * (max - min) + min;
 }
 
 
+// eslint-disable-next-line no-unused-vars
 function getRandomIntInclusive(min, max) {
   return ((Math.random() * (max - min)) + min).toFixed(5); //Максимум и минимум включаются, округление до 5 знаков
 }
+
 // Источник https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Number/toString
