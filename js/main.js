0310e10e0766df068aab5a6979e6d012272cda25
bkirill9777 <kirillbaranov087@gmail.com>
2021-10-03 19:21:48 +0300

    Исправил функции

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
-function random(min, max) {
-  return min + Math.random() * (max - min);
+function randomNumber(min, max) {
+  if (min >= max) {
+    console.error('Значение ' + max + ' должно быть больше')
+    return;
+  }
+  return Math.random() * (max - min) + min;
 }
-
 
 
 function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
-}
+  min = min;
+  max = max;
+  return ((Math.random() * (max - min)) + min).toFixed(5); //Максимум и минимум включаются, Округление до 5 знаков
 
-cansole.log(min.toFixed(5), max.toFixed(5)) // Округление до 5 знаков после запятой
 // Источник https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Number/toString
 
