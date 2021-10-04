2a6e4b18fdd724cdbcfbbc236bc41393640738a2
bkirill9777 <kirillbaranov087@gmail.com>
2021-10-04 23:28:10 +0300

    исправлены ошибки ESlint

--- js/main.js
+++ js/main.js
@@ -1,19 +1,17 @@
 // eslint-disable-next-line no-unused-vars
 function getRandomNumber(min, max) {
-  if (fmin >= fmax || fmin < 0 || fmax < 0) {
+  if (min >= max || min < 0 || max < 0) {
     return ('Задан неверный диапазон! Укажите другие числа');
   }
   return Math.random() * (max - min) + min;
 }
-
-
 // eslint-disable-next-line id-length
-function getRandomFloat (fmin, fmax, n) {
+// eslint-disable-next-line no-unused-vars
+function getRandomFloat (fmin, fmax, any) {
   if (fmin >= fmax || fmin < 0 || fmax < 0) {
     return ('Задан неверный диапазон! Укажите другие числа');
   }
-  return ((Math.random() * (fmax - fmin)) + fmin).toFixed(n);   //Максимум и минимум включаются, округление до n знаков
+  return ((Math.random() * (fmax - fmin)) + fmin).toFixed(any);   //Максимум и минимум включаются, округление до any знаков
 }
-
 // Источник https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Number/toString
