6ea54eb0656a642c046767287c0c459da47150e9
bkirill9777 <kirillbaranov087@gmail.com>
2021-10-27 23:19:18 +0300

    Исправлены недочеты

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-import {OFFERS_AMOUNT} from './variables';
-import {createOffer} from './object';
+import {OFFERS_AMOUNT} from './variables.js';
+import {createOffer} from './object.js';
 // Создания массива из 10 сгенерированных JS-объектов
 const similarOffers = Array.from({length: OFFERS_AMOUNT}, createOffer);
 window.console.log(similarOffers);
