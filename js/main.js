114e1d46beb10286a88d17221eadb995e086e0ad
bkirill9777 <kirillbaranov087@gmail.com>
2021-11-10 03:19:46 +0300

    Перенес данные в data.js добавил импорт модулей

--- js/main.js
+++ js/main.js
@@ -1,5 +1,2 @@
-import {createOffer} from './data.js';
-const OFFERS_AMOUNT = 10;
-// Создания массива из 10 сгенерированных JS-объектов
-const similarOffers = Array.from({length: OFFERS_AMOUNT}, createOffer);
-window.console.log(similarOffers);
+import './card.js';
+import './data.js';
